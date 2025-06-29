bf630648e4b8d0be7c5f1fe89853881417513942
Lucas <lucas.dias@zion.local>
2025-06-29 14:36:49 -0300

    [INIT]

---
+++ src/main/java/com/streamUI/StreamUiApplication.java
@@ -0,0 +1,13 @@
+package com.streamUI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamUiApplication.class, args);
+	}
+
+}
